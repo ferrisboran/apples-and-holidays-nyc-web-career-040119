b9bb7a55d93438bfba14739b9946916b0a59d45f
Ferris Boran <ferris.boran@gmail.com>
2019-03-13 16:52:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -42,7 +42,8 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  bbq = []
   holiday_hash.each do |season, holiday|
-    puts holiday.select { |name, supply| supply == "BBQ"}
+    bbq << holiday.select { |name, supply| supply.include?("BBQ")}
   end
 end
