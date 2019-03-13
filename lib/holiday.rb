1b309c899973cfd70f9f5f1b3b60b26adc011fe7
Ferris Boran <ferris.boran@gmail.com>
2019-03-13 16:24:42 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,7 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  holiday_hash.values
+  holiday_hash.values.capitalize!
 end
 
 def all_holidays_with_bbq(holiday_hash)
