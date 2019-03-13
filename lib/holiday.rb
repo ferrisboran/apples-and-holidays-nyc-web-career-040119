39b38abfa48850670cf83120a29a2ad15a4afc89
Ferris Boran <ferris.boran@gmail.com>
2019-03-13 16:55:21 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -44,8 +44,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   bbq = []
   holiday_hash.each do |season, holiday|
-    #bbq << holiday.select { |name, supply| supply.include?("BBQ")}.keys
-    [holiday.select { |name, supply| supply.include?("BBQ")}.keys].flatten
+    bbq << holiday.select { |name, supply| supply.include?("BBQ")}.keys
   end
-  #bbq.flatten
+  bbq.flatten
 end
